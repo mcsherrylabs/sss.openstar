d3d7fad326b55782b41057ef2a41d90de9623ec5
alan mcsherry <alan@mcsherrylabs.com>
2016-07-21 18:27:35 +0100

    documentation

--- sss.asado-common/src/test/scala/sss/asado/crypto/CBCEncryptionSpec.scala
+++ sss.asado-common/src/test/scala/sss/asado/crypto/CBCEncryptionSpec.scala
@@ -16,6 +16,7 @@ class CBCEncryptionSpec extends FlatSpec with Matchers with ByteArrayComparisonO
     val key = "coulsdfsfdIBEanmorera"
     val value = "randonw mesafsd s;klf ;lk ;lsdkf ;sldkf ;sldkf;aqkq;wkemq"
     val iv = CBCEncryption.newInitVector
+    println(s"CBC STRING IV ${iv}")
     val encrypted = CBCEncryption.encrypt(key, value, iv)
     assert(new String(encrypted, StandardCharsets.UTF_8) != value)
     assert(new String(encrypted, StandardCharsets.UTF_8) != key)
@@ -32,6 +33,7 @@ class CBCEncryptionSpec extends FlatSpec with Matchers with ByteArrayComparisonO
   val key = "codfsfdIBEanmoreraasdadsads"
   val value = "randonw mesafsd s;klf ;asdasdadlk asdad;lsdkf ;sldkf ;sldkf;aqkq;wkemq"
   val iv = CBCEncryption.newInitVector
+  println(s"CBC BYTE IV ${iv}")
   val encrypted = CBCEncryption.encrypt(key, value.getBytes, iv)
   assert(new String(encrypted, StandardCharsets.UTF_8) != value)
   assert(new String(encrypted, StandardCharsets.UTF_8) != key)
