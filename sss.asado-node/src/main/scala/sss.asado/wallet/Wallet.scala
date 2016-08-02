45c3b4761731c41f48e2c9d9201391488257f722
alan mcsherry <alan@mcsherrylabs.com>
2016-08-02 20:18:58 +0100

    Backup

--- sss.asado-node/src/main/scala/sss.asado/wallet/Wallet.scala
+++ sss.asado-node/src/main/scala/sss.asado/wallet/Wallet.scala
@@ -125,9 +125,12 @@ class Wallet(identity: NodeIdentity,
 
   private[wallet] def unSpentOpt(lodgement: Lodgement, atBlockHeight: Long): Option[UnSpent] = {
 
+    log.info(s"Is ${lodgement.txIndex.txId.asHexStr} unspent?")
     balanceLedger.entry(lodgement.txIndex) match {
       case Some(txOut) => txOut.encumbrance match {
         case SinglePrivateKey(pKey, minBlockHeight) =>
+          log.info(s"${lodgement.txIndex.txId.asHexStr} is a SinglePrivateKey with minBlkH $minBlockHeight")
+          if(identityServiceQuery.identify(pKey).isDefined) log.info("Identitified ")
           identityServiceQuery.identify(pKey).flatMap { acc =>
             if (acc.identity == identity.id && minBlockHeight <= atBlockHeight) Option(UnSpent(lodgement.txIndex, txOut))
             else None
